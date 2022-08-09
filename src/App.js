6b055724db4a8ed3d355bc003e7a018450ee0fc3
numrajaved321 <numrajaved321@gmail.com>
2022-08-09 16:49:51 +0500

    my commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import './App.css';
+import About from "./Pages/About";
+import Dasboard from "./Pages/Dasboard";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Error from "./Pages/Error";
+import Products from "./Pages/Products";
+import SharedLayout from "./Pages/SharedLayout";
+import SingleProduct from "./Pages/SingleProduct";
+import ProtectedRoute from './Pages/ProtectedRoute';
+import Protected from './Pages/Protected';
+import { Authprovider } from "./Pages/Auth";
 
 function App() {
+  const [user, setUser] = React.useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <BrowserRouter>
+    
+   <Routes>
+   
+    <Route path='/' element={<SharedLayout />} >
+    <Route
+            path='dashboard'
+            element={
+              <ProtectedRoute user={user}>
+                <Dasboard user={user} />
+              </ProtectedRoute>
+            }
+          />
+          <Route index element={<Protected Component={Home}/>} />
+         
+    <Route path='About' element={<div><About /> </div>} />
+    <Route path='Dashboard' element={<div><Dasboard user={user} /> </div>} />
+    <Route index element={<div><Home /> </div>} />
+    <Route path='Login' element={<div><Login setUser={setUser} /> </div>} />
+    <Route path='*' element={<div><Error /> </div>} />
+    <Route path='Dashboard'element={<ProtectedRoute user={user}> <Dasboard user={user} /></ProtectedRoute>}/>
+    <Route path='Products' element={<ProtectedRoute user={user}> <Products user={user}/></ProtectedRoute>} />
+    <Route path='products/:productId' element={<SingleProduct />} />
+    
+   
+   
+   </Route>
+   </Routes>
+   </BrowserRouter>
+   
+  
   );
 }
 
